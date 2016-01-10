604866a8e63eb70dd1e68cd794d2e57794c07e9e
Sam Hausman <sam.hausman@berkeley.edu>
2016-01-10 15:42:01 -0800

    Final 1.1 update

--- thermometer.c
+++ thermometer.c
@@ -11,6 +11,9 @@ main()
     upper = 300;
     step = 20;
 
+    printf("Fahrenheit-Celsius\n");
+    printf("Conversion Table\n");
+
     fahr = lower;
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr - 32.0);
